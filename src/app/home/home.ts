8604b6060eb934c06774c17b9e53c40dc0b86a1a
paulotruly <paulomsmsms@gmail.com>
2025-10-08 14:25:20 -0300

    comecandocabecalho

---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Cabecalho } from '../cabecalho/cabecalho';
+
+@Component({
+  selector: 'app-home',
+  imports: [Cabecalho],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
