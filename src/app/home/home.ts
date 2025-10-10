cdfd71abef9f56dec22116c7e3b97ed234b7784d
paulotruly <paulomsmsms@gmail.com>
2025-10-10 12:03:07 -0300

    componentedestaque

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Cabecalho } from '../cabecalho/cabecalho';
+import { MainCard } from '../main-card/main-card';
 
 @Component({
   selector: 'app-home',
-  imports: [Cabecalho],
+  imports: [Cabecalho, MainCard],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
---
+++ src/app/main-card/main-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-card',
+  imports: [],
+  templateUrl: './main-card.html',
+  styleUrl: './main-card.css'
+})
+export class MainCard {
+
+}
