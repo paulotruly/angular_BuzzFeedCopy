195fd86ed62410b8c6f6452406ede153777c2f51
paulotruly <paulomsmsms@gmail.com>
2025-10-14 11:53:27 -0300

    adicionandodinamismo

--- src/app/main-content/main-content.ts
+++ src/app/main-content/main-content.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { QuizCard } from '../quiz-card/quiz-card';
+import * as dadosQuizzes from '../../../public/dados.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-content',
-  imports: [QuizCard],
+  standalone: true,
+  imports: [QuizCard, CommonModule],
   templateUrl: './main-content.html',
   styleUrl: './main-content.css'
 })
 export class MainContent {
-
+  quizzes = dadosQuizzes.quizzes;
 }
--- src/app/quiz-card/quiz-card.ts
+++ src/app/quiz-card/quiz-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-card',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './quiz-card.css'
 })
 export class QuizCard {
-
+  @Input() dados: any;
 }
