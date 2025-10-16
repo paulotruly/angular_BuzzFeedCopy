d241203a06f17fb4c3b2b113cf5588b720002681
paulotruly <paulomsmsms@gmail.com>
2025-10-16 12:25:50 -0300

    criandocomponentequiz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
+import { QuizPage } from './quiz-page/quiz-page';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    { path: '', component: Home},
+    {path: '', component: Home},
+    {path: 'quizpage', component: QuizPage}
 ];
 
 @NgModule({
---
+++ src/app/quiz-page/quiz-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Cabecalho } from '../cabecalho/cabecalho';
+
+@Component({
+  selector: 'app-quiz-page',
+  imports: [Cabecalho],
+  templateUrl: './quiz-page.html',
+  styleUrl: './quiz-page.css'
+})
+export class QuizPage {
+
+}
