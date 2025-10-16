761630953d313081b2dfb63ec877d2b76e1cb5b7
paulotruly <paulomsmsms@gmail.com>
2025-10-16 11:36:42 -0300

    logicarotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],  // Configura o roteamento
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
